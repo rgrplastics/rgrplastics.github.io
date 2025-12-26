c2436b53be426d587acba1072f95d7d3d6f5cc99
Prabhu Prabhakaran <p.r.c.prabhu@gmail.com>
2025-12-26 16:31:39 +0530

    updated

--- js/data.js
+++ js/data.js
@@ -1,5 +1,3 @@
-let positionSlideStart = false;
-let position_count = 1;
 let addclassID;
 let addclassData;
 var ourProductsDataHTML = "";
@@ -8,39 +6,28 @@ var ourProductsIDHTML = "";
 var myPositions = JSON.parse(products);
 
 for (let i = 0; i < myPositions.length; i++) {
-  if (i == 0) {
-    addclassID = 'class="active"';
-    addclassData = 'class="item active"';
-  } else {
-    addclassID = "";
-    addclassData = 'class="item"';
-  }
-  ourProductsIDHTML += '<li data-target="#myCarousel" data-slide-to="' + i + '" ' + addclassID + "></li>";
-  ourProductsDataHTML +=
-    "<div " +
-    addclassData +
-    ">" +
-    '<div class="row" style="padding:20px">' +
-    '<div class="col-md-2"> </div> <div class="col col-md-4" style="text-align:center">' +
-    '<img  src="https://source.unsplash.com/random/250x300" alt="250x300" > ' +
-    "</div>" +
-    '<div class="col col-md-4">' +
-    myPositions[i].product_code +
-    myPositions[i].name +
-    "<b> Height : " +
-    myPositions[i].height +
-    " Diameter : " +
-    myPositions[i].diameter +
-    " Width : " +
-    myPositions[i].weight +
-    "</b>" +
-    " Colours :" +
-    myPositions[i].colours +
-    " Material :" +
-    myPositions[i].material +
-    " Grade :" +
-    myPositions[i].grade +
-    "</div> <div class='col-md-2'> </div>" +
-    "</div>" +
-    "</div>";
+  addclassID = i === 0 ? 'class="active"' : "";
+  addclassData = i === 0 ? 'class="item active"' : 'class="item"';
+
+  ourProductsIDHTML += `<li data-target="#myCarousel" data-slide-to="${i}" ${addclassID}></li>`;
+
+  ourProductsDataHTML += `
+    <div ${addclassData}>
+      <div class="row" style="padding:20px">
+        <div class="col-md-2"></div>
+        <div class="col col-md-4" style="text-align:center">
+          <img src="https://source.unsplash.com/random/250x300?product" alt="${myPositions[i].name || 'Product'} container" style="max-width:100%; height:auto;">
+        </div>
+        <div class="col col-md-4">
+          <h4>${myPositions[i].product_code}</h4>
+          <p><strong>Name:</strong> ${myPositions[i].name || 'N/A'}</p>
+          <p><strong>Dimensions:</strong> Height: ${myPositions[i].height}mm, Diameter: ${myPositions[i].diameter}mm</p>
+          <p><strong>Weight:</strong> ${myPositions[i].weight}g</p>
+          <p><strong>Colours:</strong> ${myPositions[i].colours.join(", ")}</p>
+          <p><strong>Material:</strong> ${myPositions[i].material.join(", ")}</p>
+          <p><strong>Grade:</strong> ${myPositions[i].grade.join(", ")}</p>
+        </div>
+        <div class="col-md-2"></div>
+      </div>
+    </div>`;
 }
