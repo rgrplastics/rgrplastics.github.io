73373d67586fa0b7df765741381caa4a33fdc4ce
Prabhu Prabhakaran <p.r.c.prabhu@gmail.com>
2026-09-15 23:07:56 +0530

    updated script

---
+++ js/config.js
@@ -0,0 +1,6 @@
+window.RGR_CONFIG = Object.freeze({
+    recaptchaSiteKey: '6LfGDbstAAAAAH3qVI20rVVthN-SNFmB_BOTk03v',
+    supabaseTableUrl: 'https://wxszbebddngjpyqgakdd.supabase.co/rest/v1/enquiry',
+    supabaseAnonKey: 'sb_publishable_W4bcyTGDbhQv-DRARj7PKA_pDYLoknm',
+    whatsappNumber: '919500369437'
+});
